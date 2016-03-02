aea28348973f1f481374a0037a1322f32b8f1c95
taoliu <taoliu@myemotibot.com>
2016-03-02 17:37:11 +0800

    ss

--- src/main/java/com/emotibot/neo4jprocess/EmotibotNeo4jConnection.java
+++ src/main/java/com/emotibot/neo4jprocess/EmotibotNeo4jConnection.java
@@ -13,13 +13,11 @@ public class EmotibotNeo4jConnection {
 	public EmotibotNeo4jConnection(String driver, String ip, int port, String usr, String pwd) {
 		try {
 			Class.forName(driver);
-
 			// org.neo4j.jdbc.Driver
 			// Connect
 			Properties properties = new Properties();
 			properties.put("user", usr);
 			properties.put("password", pwd);
-
 			// setNeo4jConnnection(new Driver().connect("jdbc:neo4j://" + ip + ":" + port + "/", properties));
 			// Neo4jConnection.getConnection("jdbc:neo4j://192.168.1.81:7474/");
 			conn = new Driver().connect("jdbc:neo4j://192.168.1.81:7474/", properties);
