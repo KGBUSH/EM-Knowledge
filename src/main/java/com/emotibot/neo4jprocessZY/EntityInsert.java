07b06d8fead02e0a334f9a8c7a0682dd13c956e8
cloudy <cloudy@cloudydeMBP.lan>
2016-03-03 14:50:05 +0800

    zhouyun

---
+++ src/main/java/com/emotibot/neo4jprocessZY/EntityInsert.java
@@ -0,0 +1,17 @@
+package com.emotibot.neo4jprocessZY;
+
+public class EntityInsert implements GraphOperator {
+
+	@Override
+	public void insertGraph() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void traversalGraph() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/emotibot/neo4jprocessZY/GraphOperator.java
@@ -0,0 +1,12 @@
+package com.emotibot.neo4jprocessZY;
+
+public interface GraphOperator {
+/**
+ * 
+ */
+	void insertGraph();
+	/**
+	 * 
+	 */
+	void traversalGraph();
+}
