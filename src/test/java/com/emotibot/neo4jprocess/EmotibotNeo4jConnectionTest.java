fbc0caa5c5d93e5a5ae7d100d0ae9847e013ec5d
zuquan <quanzu@emotibot.com.cn>
2016-03-04 10:55:27 +0800

    update test file

--- src/test/java/com/emotibot/neo4jprocess/EmotibotNeo4jConnectionTest.java
+++ src/test/java/com/emotibot/neo4jprocess/EmotibotNeo4jConnectionTest.java
@@ -15,24 +15,24 @@ public class EmotibotNeo4jConnectionTest {
 					configManager.getNeo4jServerIp(), configManager.getNeo4jServerPort(),
 					configManager.getNeo4jUserName(), configManager.getNeo4jPasswd());
 			Statement stmt = conn.getNeo4jConnnection().createStatement();
-			String query = "MATCH (ee:Person{born:1964}) RETURN ee";
+			String query = "MATCH (ee:Person{born:1964}) RETURN id(ee) as id, ee";
 			ResultSet rs = stmt.executeQuery(query);
 			// ResultSet rs = stmt.executeQuery("MATCH (ee:Person) WHERE ee.name
 			// = \'Keanu Reeves\' RETURN ee;");
 
 			while (rs.next()) {
 				System.out.println("test1");
+//				Map<String, Object> e = (Map<String, Object>) rs.getObject("id");
 				Map<String, Object> e = (Map<String, Object>) rs.getObject("ee");
-				System.out.println("name: " + e.get("name") + ", born: " + e.get("born"));
-
-				System.out.println("test2");
-				if (rs.next()) {
-					Map<String, Object> t = (Map<String, Object>) rs.getObject("ee");
-					for (String key : t.keySet()) {
-						System.out.println(key + "  " + t.get(key));
-					}
-				}
+				System.out.println("id: " + rs.getObject("id") + ", name: " + e.get("name") + ", born: " + e.get("born"));
 			}
+
+			System.out.println("test2");
+			Map<String, Object> t = (Map<String, Object>) rs.getObject("ee");
+			for (String key : t.keySet()) {
+				System.out.println(key + "  " + t.get(key));
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
