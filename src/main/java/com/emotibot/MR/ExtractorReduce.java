23593f14e514ab39388d96c2c02cca326d6d3049
taoliu <taoliu@myemotibot.com>
2016-03-21 14:27:52 +0800

    mapreduce

--- src/main/java/com/emotibot/MR/ExtractorReduce.java
+++ src/main/java/com/emotibot/MR/ExtractorReduce.java
@@ -5,6 +5,7 @@
  * Primary Owner: taoliu@emotibot.com.cn
  */
 package com.emotibot.MR;
+
 import java.io.IOException;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -32,24 +33,23 @@ import com.emotibot.neo4jprocess.Neo4jConfigBean;
 import com.emotibot.neo4jprocess.Neo4jDBManager;
 import com.emotibot.util.Neo4jResultBean;
 
-public  class ExtractorReduce extends
-    Reducer<ImmutableBytesWritable, Text, Writable, Put> {
-    public static ImmutableBytesWritable puttable = new ImmutableBytesWritable();
-    public static String outputTableName = "";
-    public static String type="";
-    public static EmotibotNeo4jConnection conn=null;
-  @Override
-  public void setup(Context context) 
-  {
-	  type=context.getConfiguration().get("type");
-	  outputTableName=context.getConfiguration().get("destTable");
-      puttable = new ImmutableBytesWritable(Bytes.toBytes(outputTableName));  
-      ///////
-	     String DriverName= context.getConfiguration().get("DriverName");
-	     String Ip= context.getConfiguration().get("Ip");
-	     String Password= context.getConfiguration().get("Password");
-	     int Port= context.getConfiguration().getInt("Port", 0);
-	     String User= context.getConfiguration().get("User");	     
+public class ExtractorReduce extends Reducer<ImmutableBytesWritable, Text, Writable, Put> {
+	public static ImmutableBytesWritable puttable = new ImmutableBytesWritable();
+	public static String outputTableName = "";
+	public static String type = "";
+	public static EmotibotNeo4jConnection conn = null;
+
+	@Override
+	public void setup(Context context) {
+		type = context.getConfiguration().get("type");
+		outputTableName = context.getConfiguration().get("destTable");
+		puttable = new ImmutableBytesWritable(Bytes.toBytes(outputTableName));
+		///////
+		String DriverName = context.getConfiguration().get("DriverName");
+		String Ip = context.getConfiguration().get("Ip");
+		String Password = context.getConfiguration().get("Password");
+		int Port = context.getConfiguration().getInt("Port", 0);
+		String User = context.getConfiguration().get("User");
 		Neo4jConfigBean neo4jConfigBean = new Neo4jConfigBean();
 		neo4jConfigBean.setDriverName(DriverName);
 		neo4jConfigBean.setIp(Ip);
@@ -57,31 +57,30 @@ public  class ExtractorReduce extends
 		neo4jConfigBean.setPort(Port);
 		neo4jConfigBean.setUser(User);
 		Neo4jDBManager neo4jDBManager = new Neo4jDBManager(neo4jConfigBean);
-        conn=neo4jDBManager.getConnection();
-  }
+		conn = neo4jDBManager.getConnection();
+	}
 
+	@Override
+	protected void reduce(ImmutableBytesWritable key, Iterable<Text> values, Context context)
+			throws IOException, InterruptedException {
+		try {
+			//String pageinfo = "";
+			for (Text value : values) {
+				if (type.contains("Neo4j")) {
+					String query = value.toString();
+					System.err.println("query=" + query);
+					if (conn != null) {
+						Neo4jResultBean bean = conn.executeCypherSQL(query);
+						System.err.println(bean.toString());
+					}
+				}
+				if (type.contains("Solr")) {
 
-  @Override
-  protected void reduce(ImmutableBytesWritable key,
-      Iterable<Text> values, Context context) throws IOException,
-      InterruptedException {
-    String pageinfo = "";
-    for (Text value : values) {
-		if(type.contains("Neo4j"))
-		{
-			String query=value.toString();
-			System.err.println("query="+query);
-			if(conn!=null) 
-			{
-				Neo4jResultBean bean=conn.executeCypherSQL(query);
-				System.err.println(bean.toString());
+				}
 			}
-		}
-		if(type.contains("Solr"))
-		{
-			
-		}
-    }
-  }
-}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 
+		}
+	}
+}
