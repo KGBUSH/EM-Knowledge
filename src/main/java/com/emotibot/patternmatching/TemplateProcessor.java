66e0aaa10e9bdd228008c0191c4be952e3d55f9f
zuquan <quanzu@emotibot.com.cn>
2016-03-25 11:23:46 +0800

    to test if it is an introduction-asking sentence

--- src/main/java/com/emotibot/patternmatching/TemplateProcessor.java
+++ src/main/java/com/emotibot/patternmatching/TemplateProcessor.java
@@ -6,7 +6,7 @@ package com.emotibot.patternmatching;
  *
  * copy from Yongning's template module (ruleEngine) with small modificaiton by Quan Zu
  */
-	
+
 import java.util.List;
 
 import com.emotibot.nlp.NLPFlag;
@@ -27,76 +27,83 @@ public class TemplateProcessor extends AbstractAIMLEngine {
 			rs += s;
 		}
 
-//		System.out.println("input is " + str + ", output is " + rs);
+		// System.out.println("input is " + str + ", output is " + rs);
 		return rs;
 	}
-	
 
 	public String selectiveQuestionProcess(String sentence) {
 		String processedQ = insertSpace2Chinese(sentence);
 		String type = chatSession.multisentenceRespond(processedQ);
-		System.out.println("Sentence = "+sentence+"\n ProcessQ = "+processedQ+"\n type 1 = "+type);
+		System.out.println("Sentence = " + sentence + "\n ProcessQ = " + processedQ + "\n type 1 = " + type);
 		type = removeSpace(type);
-		System.out.println("type 2 = "+type);
+		System.out.println("type 2 = " + type);
 		return type;
 	}
 
-	public String inputSentenceRewrite(String sentence) {
+	public String process(String sentence) {
 		String processedQ;
-//		NLPResult tnode = NLPSevice.ProcessSentence(sentence, NLPFlag.all.getValue());
-//		List<Term> segword = tnode.getWordPos();
-//		CoNLLSentence resetence = tnode.getReCoNLLSentence();
-//		CoNLLWord[] reCoNLLWords = resetence.word;
-//		for (CoNLLWord word : reCoNLLWords) {
-//			System.out.print("LEMMA:" + word.LEMMA + " NAME:" + word.NAME + "		 POSTAG:" + word.POSTAG + " LEVEL:"
-//					+ word.HEAD.ID + " HEAD:" + word.HEAD.LEMMA + "\n");
-//		}
-//		StringBuilder ss = new StringBuilder();
-//		for (Term t : segword) {
-//			if (t.nature.toString().startsWith("n")) {
-//				ss.append("##");
-//				ss.append(" <pos>n</pos>");
-//			}
-//			ss.append(t.word);
-			
-//			ss.append(" <pos>");						
-//			if (t.nature.toString().startsWith("a"))
-//				ss.append("a");
-//			else if (t.nature.toString().startsWith("n"))
-//				ss.append("n");
-//			else if (t.nature.toString().startsWith("v"))
-//				ss.append("v");
-//			else
-//				ss.append(t.nature.toString());			
-//			ss.append("</pos> ");	
-//		}
-		
+		// NLPResult tnode = NLPSevice.ProcessSentence(sentence,
+		// NLPFlag.all.getValue());
+		// List<Term> segword = tnode.getWordPos();
+		// CoNLLSentence resetence = tnode.getReCoNLLSentence();
+		// CoNLLWord[] reCoNLLWords = resetence.word;
+		// for (CoNLLWord word : reCoNLLWords) {
+		// System.out.print("LEMMA:" + word.LEMMA + " NAME:" + word.NAME + "
+		// POSTAG:" + word.POSTAG + " LEVEL:"
+		// + word.HEAD.ID + " HEAD:" + word.HEAD.LEMMA + "\n");
+		// }
+		// StringBuilder ss = new StringBuilder();
+		// for (Term t : segword) {
+		// if (t.nature.toString().startsWith("n")) {
+		// ss.append("##");
+		// ss.append(" <pos>n</pos>");
+		// }
+		// ss.append(t.word);
+
+		// ss.append(" <pos>");
+		// if (t.nature.toString().startsWith("a"))
+		// ss.append("a");
+		// else if (t.nature.toString().startsWith("n"))
+		// ss.append("n");
+		// else if (t.nature.toString().startsWith("v"))
+		// ss.append("v");
+		// else
+		// ss.append(t.nature.toString());
+		// ss.append("</pos> ");
+		// }
+
 		processedQ = insertSpace2Chinese(sentence);
 		String type = chatSession.multisentenceRespond(processedQ);
-		System.out.println("Sentence = "+sentence+"\n ProcessQ = "+processedQ+"\n type 1 = "+type);
-		
+		System.out.println("Sentence = " + sentence + "\n ProcessQ = " + processedQ + "\n type 1 = " + type);
+
 		type = removeSpace(type);
-		System.out.println("type 2 = "+type);
+		System.out.println("type 2 = " + type);
 		return type;
 	}
 
 	public static void main(String[] args) {
-		TemplateProcessor sentenceTypeClassifier = new TemplateProcessor("KG_Pre");
-		String str = "姚明有没有老婆";
-		System.out.println("===="+sentenceTypeClassifier.inputSentenceRewrite(str));
+		String str = "姚明是";
+		TemplateProcessor introTemplate = new TemplateProcessor("Post");
+		System.out.println("====" + introTemplate.process(str));
 
-//		SentenceTypeClassifier sentenceTypeClassifier = new SentenceTypeClassifier("Knowledge");
-//		String str = "姚明多重";
-//		System.out.println("===="+sentenceTypeClassifier.inputSentenceRewrite(str));
-		
-		
-//		String str1 = "## 姚 明 <pos>nr</pos> 属 什 么";
-//		String str2 = "## 姚 明 <type>entity</type> 属 什 么";
-//		System.out.println("@@@@==="+sentenceTypeClassifier.chatSession.multisentenceRespond(str2));
-		
-//		System.out.println("1=" + sentenceTypeClassifier.getSentenceType("姚明有多重"));
-//		System.out.println("1=" + sentenceTypeClassifier.getSentenceType("## 姚 明 <pos>n</pos> 属 什 么"));
-		//		System.out.println("3=" + sentenceTypeClassifier.getSentenceType("姚明属什么"));
+		TemplateProcessor selectTemplate = new TemplateProcessor("Pre");
+		System.out.println("====" + selectTemplate.process(str));
+
+		// SentenceTypeClassifier sentenceTypeClassifier = new
+		// SentenceTypeClassifier("Knowledge");
+		// String str = "姚明多重";
+		// System.out.println("===="+sentenceTypeClassifier.inputSentenceRewrite(str));
+
+		// String str1 = "## 姚 明 <pos>nr</pos> 属 什 么";
+		// String str2 = "## 姚 明 <type>entity</type> 属 什 么";
+		// System.out.println("@@@@==="+sentenceTypeClassifier.chatSession.multisentenceRespond(str2));
+
+		// System.out.println("1=" +
+		// sentenceTypeClassifier.getSentenceType("姚明有多重"));
+		// System.out.println("1=" + sentenceTypeClassifier.getSentenceType("##
+		// 姚 明 <pos>n</pos> 属 什 么"));
+		// System.out.println("3=" +
+		// sentenceTypeClassifier.getSentenceType("姚明属什么"));
 		// System.out.println("1=" +
 		// sentenceTypeClassifier.getSentenceType("姚明有多重呢"));
 		// System.out.println("2=" +
