9289bd3abbf576b12f2bec8408c6e820c881f369
zuquan <quanzu@emotibot.com.cn>
2016-04-08 18:39:06 +0800

    add CUBean

---
+++ src/main/java/com/emotibot/util/CUBean.java
@@ -0,0 +1,41 @@
+package com.emotibot.util;
+
+public class CUBean {
+	private String text;
+	private String questionType;
+	private String score;
+	private String uniqueID;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+	public String getUniqueID() {
+		return uniqueID;
+	}
+
+	public void setUniqueID(String uniqueID) {
+		this.uniqueID = uniqueID;
+	}
+
+}
