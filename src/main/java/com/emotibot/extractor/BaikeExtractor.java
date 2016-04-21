f693b9cc3ad31d7886ef5190fc5881c318f270c4
taoliu <taoliu@myemotibot.com>
2016-04-21 11:37:51 +0800

    多义词 同义词信息

--- src/main/java/com/emotibot/extractor/BaikeExtractor.java
+++ src/main/java/com/emotibot/extractor/BaikeExtractor.java
@@ -211,7 +211,7 @@ public class BaikeExtractor extends Extractor {
 	//http://baike.baidu.com/link?url=72qLVN_ClKpxrX47ZOyTzAprqBQdLy234q5PbfAk1Y5pVi7a0VJrZAGq1KJ1z61YcYQDnlWrnDvdcm1yVzJBxa
 	public static void main(String args[])
 	{
-		String path="/Users/Elaine/Documents/workspace/html/cluo";
+		String path="/Users/Elaine/Documents/workspace/html/xihanmulian";
 		String html=Tool.getFileContent(path);
 		Extractor ex = new BaikeExtractor(html);
 		PageExtractInfo info = ex.ProcessPage();
@@ -227,7 +227,5 @@ public class BaikeExtractor extends Extractor {
         }
 		System.err.println("getTongyici.........."+info.getTongyici());
 		System.err.println("isDuoyici.........."+info.isDuoyici());
-
 	}
-
 }
