947fbe694b7500c70ddbade9abe3ec15455d5941
taoliu <taoliu@myemotibot.com>
2016-04-24 09:32:04 +0800

    weka

--- src/main/java/com/emotibot/weka/PreProcess.java
+++ src/main/java/com/emotibot/weka/PreProcess.java
@@ -1,5 +1,46 @@
 package com.emotibot.weka;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+import com.emotibot.util.Tool;
+
 public class PreProcess {
+	
+	public static void ProduceArff(String dataFile) throws IOException
+	{
+		if(Tool.isStrEmptyOrNull(dataFile)) return ;
+		FileWriter f = new FileWriter("tag.arff",true);
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("@attribute class {");
+		for(String key:TagCommon.DomainNames.keySet())
+		{
+			buffer.append(key+",");
+		}
+		String classes = buffer.toString().trim();
+		classes=classes.substring(0, classes.length()-1);
+		classes=classes+"}";
+		f.write(classes+"\r\n");
+		//@data
+		f.write("@data"+"\r\n");
+
+		Vector<String> lines = Tool.getFileLines(dataFile);
+		for(String line:lines)
+		{
+			line=line.replaceAll("Weka:", "");
+			String[] arr = line.split("###");
+			String tags = arr[0].trim();
+			String domain = arr[1].trim();
+			f.write("‘"+tags+"’,"+domain+"\r\n");
+		}
+		f.close();
+		
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		ProduceArff("weka.txt");
+	}
 
 }
---
+++ src/main/java/com/emotibot/weka/TagCommon.java
@@ -0,0 +1,33 @@
+package com.emotibot.weka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TagCommon {
+	
+	public static Map<String,String> DomainNames;
+	static
+	{
+		DomainNames = new HashMap<>();
+		DomainNames.put("TV_series","");
+		DomainNames.put("anime","");
+		DomainNames.put("catchword","");
+		DomainNames.put("college","");
+		DomainNames.put("computer_game","");
+		DomainNames.put("cosmetics","");
+		DomainNames.put("delicacy","");
+		DomainNames.put("digital_product","");
+		DomainNames.put("figure","");
+		DomainNames.put("major","");
+		DomainNames.put("movie","");
+		DomainNames.put("novel","");
+		DomainNames.put("sports","");
+		DomainNames.put("sports_organization","");
+		DomainNames.put("tourism","");
+		DomainNames.put("varity_show","");
+		DomainNames.put("economy","");
+		DomainNames.put("medical_treatment","");
+	}
+
+
+}
