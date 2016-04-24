b78a1eda692e58d15208d229441ce9f42c02566e
taoliu <taoliu@myemotibot.com>
2016-04-24 16:46:01 +0800

    贝叶斯 分类

--- src/main/java/com/emotibot/weka/WekaTrain.java
+++ src/main/java/com/emotibot/weka/WekaTrain.java
@@ -14,30 +14,45 @@ import weka.filters.unsupervised.attribute.StringToWordVector;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Map.Entry;
+
+import com.emotibot.util.Tool;
 public class WekaTrain {
+	protected static Instances train=null;
+	protected static Instances test=null;
+	protected static Classifier m_Classifier = null;
+	protected static Evaluation eval=null;
+	public static String Other="Other";
+    public static void Train(String ArrFile) throws Exception
+    {
+    	if(Tool.isStrEmptyOrNull(ArrFile)) return ;
+    	train = DataSource.read(ArrFile);
+	    train.setClassIndex(0);
+	    m_Classifier = new NaiveBayesUpdateable();
+	    m_Classifier.buildClassifier(train);
+    }
+    
+    public static void LoadTest(String ArrFile) throws Exception
+    {
+    	if(Tool.isStrEmptyOrNull(ArrFile)) return ;
+    	test = DataSource.read(ArrFile);
+    	test.setClassIndex(0);
+    }
 	
-	
-	public static void main(String args[]) throws Exception
-	{
-		    Instances train = DataSource.read("tag.arff");
-		    train.setClassIndex(0);
-		    Instances test = DataSource.read("tag.arff");
-		    test.setClassIndex(0);
-		    if (!train.equalHeaders(test))
-		      throw new IllegalArgumentException(
-			  "Train and test set are not compatible: " + train.equalHeadersMsg(test));
-		    
-		    // train classifier
-		    NaiveBayesUpdateable cls = new NaiveBayesUpdateable();
-		    cls.buildClassifier(train);
-            cls.buildClassifier(train);
-		    // output predictions
-		    int r=0;int all=0;
-		    System.out.println("# - actual - predicted - error - distribution");
-		    for (int i = 0; i < test.numInstances(); i++) {
-		      double pred = cls.classifyInstance(test.instance(i));
-		      double[] dist = cls.distributionForInstance(test.instance(i));
+    public static void CrossValidateModelTest() throws Exception
+    {
+	    int r=0;int all=0;
+
+	    for (int i = 0; i < test.numInstances(); i++) {
+		      double pred = m_Classifier.classifyInstance(test.instance(i));
+		     // double[] dist = m_Classifier.distributionForInstance(test.instance(i));
 		      System.out.print((i+1));
 		      System.out.print(" - ");
 		      System.out.print(test.instance(i).toString(test.classIndex()));
@@ -53,19 +68,28 @@ public class WekaTrain {
 			     System.out.print("Right"); r++;
 		      }
 		      System.out.print(" - ");
-		     // System.out.print(Utils.arrayToString(dist));
 		      System.out.println();
 		   }
-		      System.out.println("all="+all+"  r="+r+" ratio="+(r*100)/all);
-		   /* Evaluation eval=null;
-		    System.out.println("==========");
-		    for(int i=0;i<1;i++){
-		     eval=new Evaluation(test);
-		     eval.crossValidateModel(cls, test, 10, new Random(i));//实现交叉验证模型
-		    }
-		    System.out.println("==========2");
-		    System.out.println(eval.toSummaryString());//输出总结信息
-		    System.out.println(eval.toClassDetailsString());//输出分类详细信息
-		    System.out.println(eval.toMatrixString());//输出分类的混淆矩阵*/
+		   for(int i=0;i<1;i++)
+		   {
+				     eval=new Evaluation(test);
+				     eval.crossValidateModel(m_Classifier, test, 10, new Random(i));//实现交叉验证模型
+		   }
+		   System.out.println(eval.toSummaryString());//输出总结信息
+ 	       System.out.println(eval.toClassDetailsString());//输出分类详细信息
+           System.out.println(eval.toMatrixString());//输出分类的混淆矩阵
+     	   System.out.println("all="+all+"  r="+r+" ratio="+(r*100)/all);
+
+    }
+    
+
+	public static void main(String args[]) throws Exception
+	{
+		String file="tag.arff";
+		Train(file);
+		LoadTest(file);
+		CrossValidateModelTest();
+		//m_Classifier.distributionForInstance(arg0)
+       // Instances data=getStructure();  //获取instances框架
 	}
 }
