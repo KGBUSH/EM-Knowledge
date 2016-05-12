1a94227198e3e9a74200556e2691eb6191a16cf6
taoliu <taoliu@myemotibot.com>
2016-05-12 14:34:50 +0800

    add weka

--- pre_process.sh
+++ pre_process.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 cd lib  
-
-vn install:install-file -Dfile=ab-4.0.5-SNAPSHOT.jar -DgroupId=com.emotibot -DartifactId=ab -Dversion=4.0.5-SNAPSHOT -Dpackaging=jar
-
+mvn install:install-file -Dfile=ab-4.0.5-SNAPSHOT.jar -DgroupId=com.emotibot -DartifactId=ab -Dversion=4.0.5-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=hanlp-1.2.8.jar -DgroupId=com.emotibot -DartifactId=hanlp -Dversion=1.2.8 -Dpackaging=jar
-
-
+mvn install:install-file -Dfile=weka.jar -DgroupId=com.emotibot -DartifactId=weka -Dversion=3.8.0 -Dpackaging=jar
 
 
