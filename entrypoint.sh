8535eeed219f261ceb2393e8c8b93e3d88895135
taoliu <taoliu@myemotibot.com>
2016-05-16 15:57:40 +0800

    KG pre

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# import the env file from local, will use some better way later...
+#source docker/local.env
+
+# update the config using the ENV variables...
+mkdir -p $KG_PRO_DIR
+impfile="$KG_PRO_DIR/config/implication.property"
+kgfile="$KG_PRO_DIR/config/KG.property"
+
+#generat debug config properties
+
+echo "age=$KG_IMP_AGE" > $impfile
+echo "computeYear=$KG_IMP_YEAR" >> $impfile
+echo "webserver.port=$KG_RS_KG_PORT" > $kgfile
+echo "db.neo4j.server.ip=$KG_NEO4J_IP" >> $kgfile
+echo "db.neo4j.server.port=$KG_NEO4J_PORT" >> $kgfile
+echo "db.neo4j.user=$KG_NEO4J_USER" >> $kgfile
+echo "db.neo4j.password=$KG_NEO4J_PASSWD" >> $kgfile
+echo "db.neo4j.drivername=$KG_NEO4J_DRIVERNAME" >> $kgfile
+echo "index.solr.server.ip=$KG_SOLR_IP" >> $kgfile
+echo "index.solr.server.port=$KG_SOLR_PORT" >> $kgfile
+echo "index.solr.server.solrname=$KG_SOLR_NAME" >> $kgfile
+echo "tcp.server.ip=$KG_TCP_IP" >> $kgfile
+echo "tcp.server.port=$KG_TCP_PORT" >> $kgfile
+
+# Start the service
+# java -cp target/WebController-0.1-jar-with-dependencies.jar  -Ddebug.conf=$EWEB_DEBUG_FILE com.emotibot.webController.WebController $EWEB_CU_ADDR $EWEB_RC_ADDR $EWEB_PORT
+java -cp KnowledgeGraph-0.0.1.jar -Ddebug.conf=$EWEB_DEBUG_FILE  com.emotibot.WebService.WebServer $EWEB_RS_KG_PORT
