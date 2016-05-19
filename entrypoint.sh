d4c327bc635e354200d4cfca90e0e9e65481731f
taoliu <taoliu@myemotibot.com>
2016-05-19 13:01:03 +0800

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,4 +28,4 @@ echo "tcp.server.port=$KG_TCP_PORT" >> $kgfile
 ${KG_PRO_DIR}/sentiment/NE_AhoCarosick & 
 # Start the service
 # java -cp target/WebController-0.1-jar-with-dependencies.jar  -Ddebug.conf=$EWEB_DEBUG_FILE com.emotibot.webController.WebController $EWEB_CU_ADDR $EWEB_RC_ADDR $EWEB_PORT
-java -cp KnowledgeGraph-0.0.1.jar -Ddebug.conf=$EWEB_DEBUG_FILE  com.emotibot.WebService.WebServer $EWEB_RS_KG_PORT
+java -cp target/KnowledgeGraph-0.0.1.jar -Ddebug.conf=$EWEB_DEBUG_FILE  com.emotibot.WebService.WebServer $EWEB_RS_KG_PORT
