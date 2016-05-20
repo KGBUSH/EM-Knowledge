bad0a73e8e8f9c401f36b92a4e41b896baf2fbaa
William Yao <wmyao@emotibot.com.cn>
2016-05-20 19:12:53 +0800

    lint docker related files
    Summary: uncomment cached files for production
    
    Test Plan: N/A
    
    Reviewers: taoliu
    
    Reviewed By: taoliu
    
    Differential Revision: http://192.168.1.48/D43

--- docker/build.sh
+++ docker/build.sh
@@ -12,14 +12,17 @@ cd $BUILDROOT
 
 mkdir -p ./build
 wget "http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/ab-4.0.5-SNAPSHOT.jar" -O ./build/ab-4.0.5-SNAPSHOT.jar
-wget "http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/hanlp-1.2.8.jar" -O ./build/hanlp-1.2.8.jar 
+wget "http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/hanlp-1.2.8.jar" -O ./build/hanlp-1.2.8.jar
 wget "http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/weka.jar" -O ./build/weka.jar
-wget "http://docker-reg.emotibot.com.cn:50000/libs/debug-logger-1.0-SNAPSHOT.jar" -O ./build/debug-logger-1.0-SNAPSHOT.jar 
-wget http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/Hanlp.tar.gz -O Hanlp.tar.gz
-tar -zxvf Hanlp.tar.gz
-cp -r Hanlp/data .
+wget "http://docker-reg.emotibot.com.cn:50000/libs/debug-logger-1.0-SNAPSHOT.jar" -O ./build/debug-logger-1.0-SNAPSHOT.jar
+wget http://docker-reg.emotibot.com.cn:50000/modules/knowledge_graph/Hanlp.tar.gz -O ./build/Hanlp.tar.gz
+
+##tar -zxvf Hanlp.tar.gz
+##cp -r Hanlp/data .
+
 # Build docker
 cmd="docker build --no-cache -t $DOCKER_IMAGE -f $DIR/Dockerfile $BUILDROOT"
+# cmd="docker build -t $DOCKER_IMAGE -f $DIR/Dockerfile $BUILDROOT"
+
 echo $cmd
 eval $cmd
-
