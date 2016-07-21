6113d61e1356d85e39456e0ab97219612b9bec7d
taoliu <taoliu@myemotibot.com>
2016-07-21 11:56:42 +0800

    拉取日志脚本改进

--- Shell/neo4jnew.sh
+++ Shell/neo4jnew.sh
@@ -2,8 +2,8 @@
 GetDataShellPath="Shell/"
 CurrPath=`pwd`
 DATE=$(date +%Y_%m_%d_%H_%M_%S)
-ErrAllLog=$GetDataShellPath$DATE
-cat /dev/null > $ErrAllLog
+ErrAllLog=$DATE
+cat /dev/null > $GetDataShellPath$ErrAllLog
 logfile="log2"
 
 echo "GetDataShellPath="$GetDataShellPath
@@ -14,9 +14,9 @@ echo "Time="$DATE
 echo "start"
 echo "flushall" | /home/taoliu/redis/redis-3.0.0/src/redis-cli 
 ###############################################################################################
-
 /tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 1 
 id=`cat $logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 1p`
+echo "id="$id
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
@@ -24,6 +24,7 @@ cd $CurrPath
 ###############################################################################################
 /tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t22 liu Neo4j 3 
 id=`cat logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 2p`
+echo "id="$id
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
@@ -31,6 +32,7 @@ cd $CurrPath
 ###############################################################################################
 /tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 2  
 id=`cat logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 3p`
+echo "id="$id
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
