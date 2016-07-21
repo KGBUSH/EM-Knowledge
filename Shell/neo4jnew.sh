c96e1053ccce9e259e6f8cddb21f2450939bdea4
taoliu <taoliu@myemotibot.com>
2016-07-21 11:39:06 +0800

    后台拉取日志脚本 优化

--- Shell/neo4jnew.sh
+++ Shell/neo4jnew.sh
@@ -4,6 +4,7 @@ CurrPath=`pwd`
 DATE=$(date +%Y_%m_%d_%H_%M_%S)
 ErrAllLog=$GetDataShellPath$DATE
 cat /dev/null > $ErrAllLog
+logfile="log2"
 
 echo "GetDataShellPath="$GetDataShellPath
 echo "CurrPath="$CurrPath
@@ -14,25 +15,22 @@ echo "start"
 echo "flushall" | /home/taoliu/redis/redis-3.0.0/src/redis-cli 
 ###############################################################################################
 
-/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 1 > tmp
-cat tmp > all
-id=`cat tmp | grep "Submitted" | grep "application" | awk -F " " '{print $7}'`
+/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 1 
+id=`cat $logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 1p`
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
 cd $CurrPath
 ###############################################################################################
-/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t22 liu Neo4j 3 > tmp
-cat tmp >> all
-id=`cat tmp | grep "Submitted" | grep "application" | awk -F " " '{print $7}'`
+/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t22 liu Neo4j 3 
+id=`cat logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 2p`
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
 cd $CurrPath
 ###############################################################################################
-/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 2  > tmp
-cat tmp >> all
-id=`cat tmp | grep "Submitted" | grep "application" | awk -F " " '{print $7}'`
+/tmp3/linux_src/hadoop-2.6.0/bin/hadoop jar KnowledgeGraph-0.0.1.jar t11 liu Neo4j 2  
+id=`cat logfile | grep "Submitted" | grep "application" | awk -F " " '{print $7}' | sed -n 3p`
 cd $GetDataShellPath
 sh getData.sh $id
 cat Node/error >> $ErrAllLog
