0a4af917fec2d7543e6c220b1b2fa7d37d2fc7bb
Should_Not_Push_From_Me <deployer@emotibot.com>
2016-07-28 14:02:10 +0800

    Add debug logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,4 +30,4 @@ cd sentiment
 cd ..
 # Start the service
 # java -cp target/WebController-0.1-jar-with-dependencies.jar  -Ddebug.conf=$EWEB_DEBUG_FILE com.emotibot.webController.WebController $EWEB_CU_ADDR $EWEB_RC_ADDR $EWEB_PORT
-java -cp target/KnowledgeGraph-0.0.1.jar -Ddebug.conf=$EWEB_DEBUG_FILE  com.emotibot.WebService.WebServer $EWEB_RS_KG_PORT
+java -cp target/KnowledgeGraph-0.0.1.jar -Ddebug.conf=config/emotibot_debug_conf.properties com.emotibot.WebService.WebServer $EWEB_RS_KG_PORT
