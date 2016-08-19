5e20a37f60ed362690e0917b3144b02e297d40d5
tantairs <1161308098@qq.com>
2016-08-19 17:15:53 +0800

    update SynonymNoun.txt and removeableMannuallyCollected.txt

--- src/main/java/com/emotibot/understanding/IntentionClassifier.java
+++ src/main/java/com/emotibot/understanding/IntentionClassifier.java
@@ -241,10 +241,6 @@ public class IntentionClassifier {
 				tempEntity = NLPUtil.getEntitySynonymNormal(tempEntity).get(0);
 				sentence = sentence.toLowerCase().replace(entitySet.get(0), tempEntity);
 				System.out.println("Intentinon 2: 获取 Synonym case tempEntity = "+tempEntity);
-			}else if (NLPUtil.isDBEntity(tempEntity)&&NLPUtil.isASynonymEntity(tempEntity)) {
-				tempEntity = NLPUtil.getEntitySynonymNormal(tempEntity).get(0);
-				sentence = sentence.toLowerCase().replace(entitySet.get(0), tempEntity);
-				System.out.println("Intentinon 2: 优先获取 Synonym case tempEntity = "+tempEntity);
 			}else {
 				System.out.println("Intentinon 2: normal case tempEntity = "+tempEntity);
 			}
